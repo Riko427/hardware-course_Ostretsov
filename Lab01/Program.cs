34ccfd0f8fff415849ac50acc5e29d2031b77896
Riko427 <ostretsov_vl20@mail.ru>
2026-09-06 17:26:38 +0300

    Lab01: README with control questions

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -3,34 +3,6 @@ string studentGroup = "ПМБИ-261";
 string studentPLanguage = "Python";
 int expInProgram = 2;
 
-ConsoleColor[] CColors = new ConsoleColor[] {
-    ConsoleColor.Black,
-    ConsoleColor.DarkBlue, 
-    ConsoleColor.DarkGreen, 
-    ConsoleColor.DarkCyan, 
-    ConsoleColor.DarkRed, 
-    ConsoleColor.DarkMagenta, 
-    ConsoleColor.DarkYellow, 
-    ConsoleColor.Gray, 
-    ConsoleColor.DarkGray, 
-    ConsoleColor.Blue, 
-    ConsoleColor.Green, 
-    ConsoleColor.Cyan, 
-    ConsoleColor.Red, 
-    ConsoleColor.Magenta, 
-    ConsoleColor.Yellow,
-    ConsoleColor.White
-    };
-
-System.Console.WriteLine("Список цветов: ");
-for (int i = 0; i<CColors.Length; i++) {
-    Console.ForegroundColor = CColors[i];
-    System.Console.Write($"{CColors[i]} ← ");
-    Console.ResetColor();
-    System.Console.WriteLine($"использован цвет: {CColors[i]}");
-}
-System.Console.WriteLine("Список завершён.");
-
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine("ВИЗИТКА");
 Console.ResetColor();
