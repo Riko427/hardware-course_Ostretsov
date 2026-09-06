405da29e3f071c28e0f5c1b5dff35e012d9f8e06
Riko427 <ostretsov_vl20@mail.ru>
2026-09-06 16:21:35 +0300

    Lab01 task 3: all console colors

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,41 +1,26 @@
-﻿Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine("ВИЗИТКА СИСТЕМЫ");
-Console.ResetColor();
-
-string studentName = "Острецов В. П.";
-string studentGroup = "ПМБИ-261";
-string studentPLanguage = "Python";
-int expInProgram = 2;
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("\n[Студент]");
-Console.ResetColor();
-Console.WriteLine($"Имя: {studentName}");
-Console.WriteLine($"Группа: {studentGroup}");
-Console.WriteLine($"Любимый язык программирования: {studentPLanguage}");
-Console.WriteLine($"Сколько лет программирует: {expInProgram}");
-
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("\n[Компьютер]");
-Console.ResetColor();
-Console.WriteLine($"Имя машины: {Environment.MachineName}");
-Console.WriteLine($"Пользователь: {Environment.UserName}");
-Console.WriteLine($"OC: {Environment.OSVersion}");
-Console.WriteLine($"64-битная OC: {Environment.
-Is64BitOperatingSystem}");
-Console.WriteLine($"Версия .NET: {Environment.Version}");
-
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("\n[Процессор]");
-Console.ResetColor();
-Console.WriteLine($"Логических ядер: {Environment.ProcessorCount}");
-
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("\n[Память]");
-Console.ResetColor();
-long workingSetMb = Environment.WorkingSet / 1024 / 1024;
-Console.WriteLine($"Память процесса: {workingSetMb} МБ");
-Console.WriteLine($"Размер указателя: {IntPtr.Size * 8} бит");
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("\nПрограмма выполнена успешно!");
-Console.ResetColor();
+﻿ConsoleColor[] CColors = new ConsoleColor[] {
+    ConsoleColor.Black,
+    ConsoleColor.DarkBlue, 
+    ConsoleColor.DarkGreen, 
+    ConsoleColor.DarkCyan, 
+    ConsoleColor.DarkRed, 
+    ConsoleColor.DarkMagenta, 
+    ConsoleColor.DarkYellow, 
+    ConsoleColor.Gray, 
+    ConsoleColor.DarkGray, 
+    ConsoleColor.Blue, 
+    ConsoleColor.Green, 
+    ConsoleColor.Cyan, 
+    ConsoleColor.Red, 
+    ConsoleColor.Magenta, 
+    ConsoleColor.Yellow,
+    ConsoleColor.White
+    };
+System.Console.WriteLine("Список цветов: ");
+for (int i = 0; i<CColors.Length; i++) {
+    Console.ForegroundColor = CColors[i];
+    System.Console.Write($"{CColors[i]} ← ");
+    Console.ResetColor();
+    System.Console.WriteLine($"использован цвет: {CColors[i]}");
+}
+System.Console.WriteLine("Список завершён!");
